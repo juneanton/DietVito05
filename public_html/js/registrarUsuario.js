78878f1606f1df6509f9777f793e928eebf32110
miren <miren@192.168.1.106>
2019-11-18 20:22:23 +0100

    JavaScripts

---
+++ public_html/js/registrarUsuario.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//window.addEventListener('load', inicio, false);
+//
+//function inicio() {
+//    document.getElementById('archivo').addEventListener('change', cargar, false);               
+//}
+//
+//function cargar(ev) {
+//    var arch=new FileReader();
+//    arch.addEventListener('load',leer,false);
+//    arch.readAsDataURL(ev.target.files[0]);
+//}
+//
+//function leer(ev) {
+//    document.getElementById('caja').style.backgroundImage="url('" + ev.target.result + "')";
